0a96fabf2b146c29e3f80c02cc158933aceec958
Sahid Reza <sahidreza976@gmail.com>
2026-08-31 09:31:37 +0530

    Add the full function of evaluatePostfix

--- src/main/java/org/example/Postfix_Implementation.java
+++ src/main/java/org/example/Postfix_Implementation.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Postfix_Implementation {
 
     public static int floorDiv(int a , int b){
@@ -11,6 +13,24 @@ public class Postfix_Implementation {
 
     public static int evaluatePostfix(String [] arr){
 
+        Stack<Integer>st = new Stack<>();
+        for(String token : arr){
+            // handle the negative token
+            if(Character.isDigit(token.charAt(0)) ||
+            token.length() > 1 && token.charAt(0) == '-'){
+                st.push(Integer.parseInt(token));
+            }else {
+                int val1 = st.pop();
+                int val2 = st.pop();
+
+                if(token.equals("+"))st.push(val2+val1);
+                else if(token.equals("-"))st.push(val2-val1);
+                else if(token.equals("*"))st.push(val2*val1);
+                else if(token.equals("/"))st.push(floorDiv(val2,val1));
+                else if(token.equals("^"))st.push((int)Math.pow(val2,val1));
+            }
+        }
+        return st.pop();
     }
     public static void main(String[] args) {
 
