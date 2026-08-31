5188f3ad70262a61f0b712a5e94290371db187e0
Sahid Reza <sahidreza976@gmail.com>
2026-08-31 07:22:21 +0530

    Add the constructor

--- src/main/java/org/example/Implement_Two_Stack_In_A_Array.java
+++ src/main/java/org/example/Implement_Two_Stack_In_A_Array.java
@@ -1,6 +1,21 @@
 package org.example;
 
 public class Implement_Two_Stack_In_A_Array {
+    int arr[];
+    int size;
+    int mid;
+    int top1, top2;
+
+    public Implement_Two_Stack_In_A_Array(int n) {
+        size = n;
+        arr = new int[n];
+        mid = size/2;
+        top1 = -1;
+        top2 = mid-1;
+    }
+
+
+
     public static void main(String[] args) {
 
     }
