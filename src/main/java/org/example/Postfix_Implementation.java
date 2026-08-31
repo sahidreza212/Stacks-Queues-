3fd28219f35e9f1ea7ba7a2900706a317c047fff
Sahid Reza <sahidreza976@gmail.com>
2026-08-31 09:22:44 +0530

    Add the full function of the floorDiv

--- src/main/java/org/example/Postfix_Implementation.java
+++ src/main/java/org/example/Postfix_Implementation.java
@@ -3,7 +3,10 @@ package org.example;
 public class Postfix_Implementation {
 
     public static int floorDiv(int a , int b){
-
+        if((a*b) < 0 && a % b != 0){
+            return (a/b)-1;
+        }
+        return a/b;
     }
     public static void main(String[] args) {
 
