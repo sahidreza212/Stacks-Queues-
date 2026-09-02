a30332f4905b190b9838b807f21a851089ff3246
Sahid Reza <sahidreza976@gmail.com>
2026-09-02 07:23:05 +0530

    Add the DQueNode class fir node

--- src/main/java/org/example/Implement_Stack_Queue_Using_Deque.java
+++ src/main/java/org/example/Implement_Stack_Queue_Using_Deque.java
@@ -1,6 +1,13 @@
 package org.example;
 
 public class Implement_Stack_Queue_Using_Deque {
+
+    // create the deque node
+    static  class DQueNode{
+        int value;
+        DQueNode prev;
+        DQueNode next;
+    }
     public static void main(String[] args) {
 
     }
