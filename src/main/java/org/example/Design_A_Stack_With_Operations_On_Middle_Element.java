dd236438e8cd6b3f94b3ce6fc352881652b00ecd
Sahid Reza <sahidreza976@gmail.com>
2026-09-03 08:34:02 +0530

    Add the pop function

--- src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
+++ src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
@@ -14,6 +14,14 @@ public class Design_A_Stack_With_Operations_On_Middle_Element {
             stack.add(x);
         }
 
+        // pop operation
+        int pop(){
+            if(stack.isEmpty()){
+                return -1;
+            }
+            return stack.get(stack.size()-1);
+        }
+
     }
     public static void main(String[] args) {
 
