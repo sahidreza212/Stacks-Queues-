47dc26a1d7981acc8c2c4ad6bed279b0552ccd04
Sahid Reza <sahidreza976@gmail.com>
2026-09-03 08:29:46 +0530

    Add the my stack class

--- src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
+++ src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
@@ -2,7 +2,11 @@ package org.example;
 
 public class Design_A_Stack_With_Operations_On_Middle_Element {
 
+    public static class myStack{
+
+    }
     public static void main(String[] args) {
+
 
     }
 }
