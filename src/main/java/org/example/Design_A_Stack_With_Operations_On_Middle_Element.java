0f073866175c14c922125ecc33c3762151530f49
Sahid Reza <sahidreza976@gmail.com>
2026-09-03 08:31:19 +0530

    Add the array list

--- src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
+++ src/main/java/org/example/Design_A_Stack_With_Operations_On_Middle_Element.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Design_A_Stack_With_Operations_On_Middle_Element {
 
     public static class myStack{
+
+        // take the help of ArrayList to solve this problem
+        ArrayList<Integer>stack = new ArrayList<>();
 
     }
     public static void main(String[] args) {
