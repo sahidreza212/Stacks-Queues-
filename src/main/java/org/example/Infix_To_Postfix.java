e77d30afca9b74acc0c99d4226fa015b4a9c5de0
Sahid Reza <sahidreza976@gmail.com>
2026-09-06 06:55:58 +0530

    Add the precedence full function

--- src/main/java/org/example/Infix_To_Postfix.java
+++ src/main/java/org/example/Infix_To_Postfix.java
@@ -3,6 +3,17 @@ package org.example;
 public class Infix_To_Postfix {
     static int prec(char c){
 
+        if(c == '^'){
+            return 3;
+        }
+        else if(c == '*' || c == '/') {
+            return 2;
+        } else if(c == '+' || c == '-') {
+            return 1;
+        }
+        else {
+            return -1;
+        }
     }
 
 
