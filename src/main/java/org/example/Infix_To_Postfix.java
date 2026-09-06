2deaf843375266f710201cf9d87b10b349de9819
Sahid Reza <sahidreza976@gmail.com>
2026-09-06 07:05:14 +0530

    Add the code till now

--- src/main/java/org/example/Infix_To_Postfix.java
+++ src/main/java/org/example/Infix_To_Postfix.java
@@ -27,6 +27,17 @@ public class Infix_To_Postfix {
 
         Stack<Character>st = new Stack<>();
         StringBuilder res = new StringBuilder();
+        for(int i = 0;i<s.length();i++){
+            char c = s.charAt(i);
+
+            if((c >= 'a' && c <= 'z') ||
+                    (c >= 'A' && c <= 'Z') ||
+                    (c >= '0' && c <= '9')){
+                res.append(c);
+            }
+
+
+        }
 
 
 
