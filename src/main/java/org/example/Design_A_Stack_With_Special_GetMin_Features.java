0053ce471a1530552e7b20fd13a6976950ed9e29
Sahid Reza <sahidreza976@gmail.com>
2026-09-06 08:02:54 +0530

    Add the puah function

--- src/main/java/org/example/Design_A_Stack_With_Special_GetMin_Features.java
+++ src/main/java/org/example/Design_A_Stack_With_Special_GetMin_Features.java
@@ -6,6 +6,24 @@ public class Design_A_Stack_With_Special_GetMin_Features {
     Stack<Integer>stack = new Stack<>();
     int min;
 
+    void push(int x){
+
+        // if the stack is empty
+        if(stack.isEmpty()){
+            stack.push(x);
+            min = x;
+        }
+        // if x is smaller than the min
+        else if( x < min) {
+            stack.push(2*x-min);
+            min = x;
+        }
+        // normal push
+        else {
+            stack.push(x);
+        }
+    }
+
 
 
     public static void main(String[] args) {
