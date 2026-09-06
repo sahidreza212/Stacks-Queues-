e7bfbb0cfac2af21e926fe488b8499ffa4ee9949
Sahid Reza <sahidreza976@gmail.com>
2026-09-06 07:59:06 +0530

    Add the stack

--- src/main/java/org/example/Design_A_Stack_With_Special_GetMin_Features.java
+++ src/main/java/org/example/Design_A_Stack_With_Special_GetMin_Features.java
@@ -1,6 +1,13 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Design_A_Stack_With_Special_GetMin_Features {
+    Stack<Integer>stack = new Stack<>();
+    int min;
+
+
+
     public static void main(String[] args) {
 
     }
