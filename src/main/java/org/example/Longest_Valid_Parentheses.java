475cc61a2ebb7fa868dbda5b2fdbb59cd037160d
Sahid Reza <sahidreza976@gmail.com>
2026-09-16 08:46:13 +0530

    create a stack for this question to track the opening brackets

--- src/main/java/org/example/Longest_Valid_Parentheses.java
+++ src/main/java/org/example/Longest_Valid_Parentheses.java
@@ -5,6 +5,10 @@ import java.util.Stack;
 public class Longest_Valid_Parentheses {
     public static int maxLength(String s){
 
+        Stack<Integer>st = new Stack<>();
+        st.push(-1);
+        int maxLen = 0;
+
     }
     public static void main(String[] args) {
 
