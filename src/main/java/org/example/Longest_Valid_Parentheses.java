d5c1bea536aba8f06f8a0273030445f8ea49722d
Sahid Reza <sahidreza976@gmail.com>
2026-09-16 08:44:48 +0530

    Add the maxLength function

--- src/main/java/org/example/Longest_Valid_Parentheses.java
+++ src/main/java/org/example/Longest_Valid_Parentheses.java
@@ -1,6 +1,11 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Longest_Valid_Parentheses {
+    public static int maxLength(String s){
+
+    }
     public static void main(String[] args) {
 
     }
