f29df5321a1aa7e5469aa6be1d5d7693da507192
Sahid Reza <sahidreza976@gmail.com>
2026-09-25 07:08:35 +0530

    Add the full function

--- src/main/java/org/example/Find_Duplicate_Parenthesis_or_not.java
+++ src/main/java/org/example/Find_Duplicate_Parenthesis_or_not.java
@@ -7,6 +7,25 @@ public class Find_Duplicate_Parenthesis_or_not {
 
         Stack<Character>st = new Stack<>();
         char [] str = s.toCharArray();
+        for(char ch : str){
+            if(ch == ')'){
+                char top = st.peek();
+                st.pop();
+
+                int elementInside = 0;
+                while(ch != '('){
+                    elementInside++;
+                    top = st.peek();
+                    st.pop();
+                }
+                if(elementInside < 1){
+                    return true;
+                }
+            }else {
+                st.push(ch);
+            }
+        }
+        return false;
 
     }
     public static void main(String[] args) {
