c03c4008c066190c3262b696b97afc46f01c757c
Sahid Reza <sahidreza976@gmail.com>
2026-09-25 08:13:11 +0530

    Add the full main finction

--- src/main/java/org/example/Stack_Permutation.java
+++ src/main/java/org/example/Stack_Permutation.java
@@ -37,6 +37,15 @@ public class Stack_Permutation {
     }
     public static void main(String[] args) {
 
+        int[] a = { 1,2,3 };
+        int[] b = { 3,2,1 };
+
+        if(validateOutput(a,b)){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+
 
     }
 }
