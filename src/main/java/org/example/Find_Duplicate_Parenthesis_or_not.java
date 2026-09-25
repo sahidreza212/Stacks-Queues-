3e8a2f3a6239db1124795b59f79635bdfe3147bf
Sahid Reza <sahidreza976@gmail.com>
2026-09-25 07:03:31 +0530

    Convert the String to char

--- src/main/java/org/example/Find_Duplicate_Parenthesis_or_not.java
+++ src/main/java/org/example/Find_Duplicate_Parenthesis_or_not.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Stack;
+
 public class Find_Duplicate_Parenthesis_or_not {
     static boolean findDuplicateParenthesis(String s){
+
+        Stack<Character>st = new Stack<>();
+        char [] str = s.toCharArray();
 
     }
     public static void main(String[] args) {
