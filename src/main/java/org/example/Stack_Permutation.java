c5e3736767751d16fe15f83d311692f08cb2c4c9
Sahid Reza <sahidreza976@gmail.com>
2026-09-25 08:10:45 +0530

    Add the full function of validateOutput

--- src/main/java/org/example/Stack_Permutation.java
+++ src/main/java/org/example/Stack_Permutation.java
@@ -1,10 +1,42 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Stack_Permutation {
     public static boolean validateOutput(int[] a, int[] b){
 
+        Queue<Integer>q1 = new LinkedList<>();
+        for(int i = 0; i < a.length;i++){
+            q1.add(a[i]);
+        }
+
+        Queue<Integer>q2 = new LinkedList<>();
+        for(int i = 0 ; i < b.length; i++){
+            q2.add(b[i]);
+        }
+
+        Stack<Integer>st = new Stack<>();
+
+        while(!q1.isEmpty()){
+            int ele = q1.poll();
+
+            if(ele == q2.peek()){
+                q2.poll();
+
+                while(!q2.isEmpty() && !st.isEmpty() && st.peek() == q2.peek()){
+                    st.pop();
+                    q2.poll();
+                }
+            }else {
+                st.push(ele);
+            }
+        }
+        return q2.isEmpty();
     }
     public static void main(String[] args) {
+
 
     }
 }
